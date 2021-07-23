7c35c55b804aa2da023c230a23c0e4be20978d01
Aniket Ray <aniketiq@gmail.com>
2021-07-23 16:56:42 +0530

    Graph Init and Run

--- mediapipe/examples/desktop/audio_run_graph_main.cc
+++ mediapipe/examples/desktop/audio_run_graph_main.cc
@@ -28,10 +28,10 @@ constexpr char kOutputStream[] = "output_text";
 
 ABSL_FLAG(std::string, calculator_graph_config_file, "",
           "Name of file containing text format CalculatorGraphConfig proto.");
-ABSL_FLAG(std::string, input_audio_path, "",
+ABSL_FLAG(std::string, input_side_packets, "",
           "Full path of video to load. "
           "If not provided, attempt to use a mic.");
-ABSL_FLAG(std::string, output_video_path, "",
+ABSL_FLAG(std::string, output_text_path, "",
           "Full path of where to save result (.txt). "
           "If not provided, show result in a window.");
 
@@ -40,18 +40,45 @@ absl::Status RunMPPGraph() {
     MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
         absl::GetFlag(FLAGS_calculator_graph_config_file),
         &calculator_graph_config_contents));
+
     LOG(INFO) << "Get calculator graph config contents: "
               << calculator_graph_config_contents;
     mediapipe::CalculatorGraphConfig config =
         mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(
             calculator_graph_config_contents);
+    std::map<std::string, mediapipe::Packet> input_side_packets;
+    std::vector<std::string> kv_pairs =
+        absl::StrSplit(absl::GetFlag(FLAGS_input_side_packets), ',');
+    for (const std::string& kv_pair : kv_pairs) {
+        std::vector<std::string> name_and_value = absl::StrSplit(kv_pair, '=');
+        RET_CHECK(name_and_value.size() == 2);
+        RET_CHECK(!mediapipe::ContainsKey(input_side_packets, name_and_value[0]));
+        std::string input_side_packet_contents;
+        MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
+            name_and_value[1], &input_side_packet_contents));
+        input_side_packets[name_and_value[0]] =
+            mediapipe::MakePacket<std::string>(input_side_packet_contents);
+    }
 
     LOG(INFO) << "Initialize the calculator graph.";
     mediapipe::CalculatorGraph graph;
-    MP_RETURN_IF_ERROR(graph.Initialize(config));
+    MP_RETURN_IF_ERROR(graph.Initialize(config, input_side_packets));
 
-    LOG(INFO) << "Check if audio file is provided.";
-    const bool audio_file_path = !absl::GetFlag(FLAGS_input_audio_path).empty();
+    LOG(INFO) << "Check if input side packet metadata is provided.";
+    const bool audio_file_path = !absl::GetFlag(FLAGS_input_side_packets).empty();
+    RET_CHECK(audio_file_path);
+
+    LOG(INFO) << "Check if output txt file is provided.";
+    const bool output_file_path = !absl::GetFlag(FLAGS_output_text_path).empty();
+    RET_CHECK(output_file_path);
+
+    LOG(INFO) << "Start running the calculator graph.";
+    ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
+                     graph.AddOutputStreamPoller(kOutputStream));
+    MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+    LOG(INFO) << "Start grabbing and processing frames.";
+
     return absl::OkStatus();
 }
 
